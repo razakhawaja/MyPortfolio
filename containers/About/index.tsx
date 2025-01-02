aebf9fa4bec89ac84e6d14a03da535f32cafaa5f
Raza Khawaja <31221657+razakhawaja@users.noreply.github.com>
2025-01-01 21:04:17 -0600

    about-page-fixes

--- containers/About/index.tsx
+++ containers/About/index.tsx
@@ -1,25 +1,110 @@
 import React from "react";
 import SectionHeading from "../../components/SectionHeading";
-import Markdown from "../../components/Markdown";
 
-const profileImg = require("../../public/aboutme.svg");
+const About = ({ about }: { about: string }) => {
+  const imgSrc = require("./aboutme.png");
 
-const About = ({ about }: { about: string }) => (
-  <div id="about" className="w-full py-24 flex flex-col justify-center ">
-    <SectionHeading text={"About Me"} index={1} />
-    <div className="flex flex-col-reverse lg:grid lg:grid-cols-1 lg:gap-4">
-      <div className="sm:col-start-1 sm:col-end-4 leading-normal text-blueGray-500 dark:text-gray-600 about-container">
-        <Markdown content={about} isNotFullPage />
-      </div>
-      <div className="flex justify-center items-center mb-8 sm:mt-0 sm:col-end-6 sm:col-span-2 ">
-        <img
-          alt="profile image"
-          src={profileImg}
-          className="h-auto w-auto max-w-md max-h-md rounded"
-        />
+  return (
+    <div id="about" className="w-full flex flex-col justify-center ">
+      <SectionHeading text={"About Me"} index={1} />
+      <div className="text-blueGray-500 dark:text-gray-600 about-container">
+        <div className="flex flex-col-reverse sm:flex-row justify-center items-center w-full">
+          <div className="w-50 pr-2 max-w-lg leading-relaxed text-xl text-blueGray-500">
+            I'm currently a UX Designer at{" "}
+            <a href="https://www.smithweb.com" target="_blank">
+              {" "}
+              <span
+                className="font-semibold hover:opacity-75"
+                style={{ color: "#000" }}
+              >
+                Smith & Associates
+              </span>
+            </a>
+            , based in Houston, Texas. Previously, I have been a full stack
+            Software Engineer at{" "}
+            <a href="https://www.educative.io" target="_blank">
+              {" "}
+              <span className="cursor-pointer font-semibold hover:opacity-75 gradient-purple text-gradient">
+                Educative
+              </span>
+            </a>
+            , and a Design Engineer at{" "}
+            <a href="https://www.onesignal.com" target="_blank">
+              <span
+                className="cursor-pointer font-semibold hover:opacity-75"
+                style={{ color: "#e54b4d" }}
+              >
+                OneSignal
+              </span>
+            </a>
+            . I have also worked with several research groups from various
+            universities as a UX Consultant through the{" "}
+            <a href="https://www.dvclab.net/" target="_blank">
+              DVC Lab
+            </a>{" "}
+            at Purdue University.
+            <br></br>
+            <br></br>
+            Outside of work, you will often find me either immersed in a
+            single-player video game, discussing sports a little too
+            passionately, or trying to paint (I use soft pastels!). If you're
+            interested in talking more about my work, research interests, or
+            simply want to rant about how bad Manchester United are right now,
+            send me an email over at raza.khawaja@hotmail.com.
+          </div>
+          <img
+            loading="lazy"
+            className="w-2/4 sm:h-auto sm:w-auto sm:max-w-md sm:max-h-md"
+            src={imgSrc}
+          />
+        </div>
+        <br></br>
+        ...
+        <br></br>
+        <div className="leading-relaxed text-xl text-blueGray-500">
+          If you've made it this far and aren't bored yet, here's some more info
+          about me!
+          <br></br>
+          <br></br>
+          <span className="font-medium">
+            Why did I switch from software engineering to design?
+          </span>
+          <br></br>
+          <br></br>I found that design allowed me to fully channel my creativity
+          and explore my interest in human behaviour. Designing experiences
+          brought me more joy and fulfillment than engineering them. Plus, with
+          experience and education in both fields, I also enjoy working at the
+          intersection of design and engineering.
+          <br></br>
+          <br></br>
+          <span className="font-medium">
+            What are my thoughts on AI and design?
+          </span>
+          <br></br>
+          <br></br>
+          AI can definitely be used to design better experiences and allow the
+          designer to focus more on understanding user needs and empathazing
+          with them - things that AI can not do. However, AI is innovating fast
+          so don't quote me in a few years time if a humanoid robot shows up
+          asking for your clothes, boots and motorcycle.
+          <br></br>
+          <br></br>
+          <span className="font-medium">
+            What specifically interests me about design?
+          </span>
+          <br></br>
+          <br></br>I enjoy working in ambigious spaces where I can speak to
+          people and figure out how their problems can be solved. Specifically,
+          I am most interested in cross-channel interaction design, AI's role in
+          modern UX design, and studying{" "}
+          <a href="https://www.deceptive.design/" target="_blank">
+            dark patterns
+          </a>
+          .
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default About;
--- public/info/about.md
+++ public/info/about.md
@@ -10,4 +10,23 @@ My interests revolve around UX Design & Research, Design Systems and Product Man
 
 As an engineer, I not only loved working as a technical design lead but also enjoyed engaging in the product and design discussions and enjoyed channeling my creativity there. This is why I am driven to complement my computer science background with a UX specialization. This has allowed me to find my place as a designer at the intersection of design and development.
 
-If you want to work with me, or want to grab coffee, or rant about how bad Manchester United are right now, send me an email over at raza.khawaja@hotmail.com.
+Outside of work, you will often find me either immersed in a single-player video game, discussing sports a little too passionately, or trying to paint (I use soft pastels!). If you're interested in talking more about my work, research interests, or simply want to rant about how bad Manchester United are right now, send me an email over at raza.khawaja@hotmail.com.
+
+</div>
+<img loading="lazy" class="w-2/4 sm:h-auto sm:w-auto sm:max-w-md sm:max-h-md" src="/info/aboutme.png" />
+</div>
+...
+
+If you've made it this far and aren't bored yet, here's some more info about me!
+
+<span class="font-medium">Why did I switch from software engineering to design?</span>
+
+I found that design allowed me to fully channel my creativity and explore my interest in human behaviour. Designing experiences brought me more joy and fulfillment than engineering them. Plus, with experience and education in both fields, I also enjoy working at the intersection of design and engineering.
+
+<span class="font-medium">What are my thoughts on AI and design?</span>
+
+AI can definitely be used to design better experiences and allow the designer to focus more on understanding user needs and empathazing with them - things that AI can not do. However, AI is innovating fast so don't quote me in a few years time if a humanoid robot shows up asking for your clothes, boots and motorcycle.
+
+<span class="font-medium">What specifically interests me about design?</span>
+
+I enjoy working in ambigious spaces where I can speak to people and figure out how their problems can be solved. Specifically, I am most interested in cross-channel interaction design, AI's role in modern UX design, and studying <a href="https://www.deceptive.design/" target="_blank">dark patterns</a>.
